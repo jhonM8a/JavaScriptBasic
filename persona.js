2aa98070c6a1f24077d224e7d68706e04d1e3431
jochoa <jhon.ochoa@openintl.com>
2019-05-17 14:39:01 -0500

    Se define nuevo metodo para un objeto

--- persona.js
+++ persona.js
@@ -1,12 +1,14 @@
 //definicio prototipo
 
-function Persona(nombre, apellido){
+function Persona(nombre, apellido,altura){
 	this.nombre = nombre
 	this.apellido = apellido
+	this.altura = altura
 }
 
 Persona.prototype.saludar = function() {console.log(`Hola me llamo ${this.nombre}`)}
- 
-var jhon = new Persona('Jhon', 'Ochoa'); //Crea un nuevo objeto dado un prototipo
+Persona.prototype.soyAlto = function() {console.log(`${this.altura >=1.8 ? "Soy alto":"No soy alto"}`)} 
+var jhon = new Persona('Jhon', 'Ochoa',1.7); //Crea un nuevo objeto dado un prototipo
 
 jhon.saludar()
+jhon.soyAlto()

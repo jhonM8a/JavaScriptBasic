f2f236e8887c39c42f31e0044303cb1ae1d81f79
jhonochoa <m8alemus@gmail.com>
2019-05-21 20:26:10 -0500

    Se hacen multiples Requests

--- callbacks.js
+++ callbacks.js
@@ -1,11 +1,20 @@
 const API_URL = "https://swapi.co/api/";
 const PEOPLE_URL = "people/:id";
 
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const opts = { crossDomain: true };
 
 const onPeopleResponse = function(person) {
   console.log(`Hola, yo soy ${person.name}`);
 };
 
-$.get(lukeUrl, { crossDomain: true }, onPeopleResponse);
+function obtenerPersonaje(id) {
+  const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+
+  $.get(url, { crossDomain: true }, onPeopleResponse);
+}
+/** No se sabe en que orden lleguen las respuestas
+ * por el asinconismo
+ */
+obtenerPersonaje(1);
+obtenerPersonaje(2);
+obtenerPersonaje(3);
